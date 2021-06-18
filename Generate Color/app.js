b77eb8d263241a9ebf5e26e40845ffd686cf40d6
VishalSahu <52187641+VishalSahu@users.noreply.github.com>
2021-06-18 18:22:38 +0530

    Add files via upload

---
+++ Generate Color/app.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('button');
+
+
+btn.addEventListener('click',function(){
+    const makecolour = CreateColor();
+    document.body.style.backgroundColor = makecolour;
+    document.body.style.transition = "all 1s";
+    btn.style.transition = "all 1s";
+
+})
+
+const CreateColor = () => {
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    if(r<70 || g<70 || b<70){
+    document.body.style.color = "white";
+    btn.style.color = "white";
+    }
+    return `rgb(${r},${g},${b})`;
+}
